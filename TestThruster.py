ad7850afc64fa797f5ae2e92172f718f442f039b
chihaya-kawamura <ckck4253@gmail.com>
2021-05-28 01:15:26 +0900

    1100

--- TestThruster.py
+++ TestThruster.py
@@ -54,9 +54,10 @@ if __name__ == "__main__":
         params = Params()
         sample = TestThruster(params.pin_servo_out, params.pin_thruster_out)
 
-        minPulse = 1100
+
+        minPulse = 700
         maxPulse = 2000
-        sample.thruster_pulse_width = minPulse
+        sample.thruster_pulse_width = 1100
 
         print("Initialaze Brushless Motor. Please remove the battery.")
 

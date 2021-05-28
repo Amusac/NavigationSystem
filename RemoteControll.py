a1b12a89f7d2daf0c45edc52cc11ff334fc02235
chihaya-kawamura <ckck4253@gmail.com>
2021-05-28 16:22:48 +0900

    many

---
+++ RemoteControll.py
@@ -0,0 +1,24 @@
+##!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#SolarBoatProject2021
+
+from Params import Params
+from PwmOut import PwmOut
+from PwmRead import PwmRead
+
+
+import sys
+
+class RemoteControll:
+    def __init__(self):
+        self._params = Params()
+        self._pwm_read = PwmRead(
+            self._params.pin_mode_in,
+            self._params.pin_servo_in,
+            self._params.pin_thruster_in,
+            self._params.pin_or,
+        )
+        self._pwm_out = PwmOut(
+            self._params.pin_servo_out, self._params.pin_thruster_out
+        )

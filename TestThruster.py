73bd9593924cfe7df8b112a77707d16c0bd58b88
chihaya-kawamura <ckck4253@gmail.com>
2021-05-28 16:22:54 +0900

    many

--- TestThruster.py
+++ TestThruster.py
@@ -26,7 +26,7 @@ class TestThruster:
         self.pin_servo = pin_servo
         self.pin_thruster = pin_thruster
         self.servo_pulse_width = 1500
-        self.thruster_pulse_width = 1500
+        self.thruster_pulse_width = 1100
 
         # Setup for Out
         self.pi = pigpio.pi()
@@ -53,35 +53,49 @@ if __name__ == "__main__":
     try:
         params = Params()
         sample = TestThruster(params.pin_servo_out, params.pin_thruster_out)
+        #servo
+        resolution = 80
+        pwm_range = 1900 - 1500
+        dp = pwm_range / resolution
+        servo_pulse_width = 1500
+        #thruster
         minPulse = 1100
-        maxPulse = 2000
-
-        print("Initialaze Brushless Motor. Please remove the battery.")
-
-        print("Connect the battery and press Enter after the beeping stops.")
+        maxPulse = 1900
+    
         inp = input()
+        print("Initialaze Brushless Motor. Please reconnect the battery.")
         if inp == "":
-            time.sleep(3)
+            print("Press Enter after the beeping stops.")
+        if inp == "":
+            time.sleep(1)
 
+        print('"Commands are as follaws"')    
         print('"stop"')
         print('"u" to up speed')
-        print('"d" to down speed')
-        sample.thruster_pulse_width = minPulse
+        print('"j" to down speed')
+        print('"k" to turn right')
+        print('"h" to turn left')
         print("speed = %d" % sample.thruster_pulse_width)
         while True:
             sample.update_pulse_width()
             inp = input()
-            if inp == "d":
-                sample.thruster_pulse_width -= 100
-                print("speed = %d" % sample.thruster_pulse_width)
-            elif inp == "u":
+            if inp == "u":
                 sample.thruster_pulse_width += 100  # incrementing the speed like hell
-                print("speed = %d" % sample.thruster_pulse_width)
+                print("speed = %d direction = %d" % sample.thruster_pulse_width % sample.servo_pulse_width)
+            elif inp == "j":
+                sample.thruster_pulse_width -= 100
+                print("speed = %d direction = %d" % sample.thruster_pulse_width % sample.servo_pulse_width)
+            elif inp == "k":
+                sample.thruster_pulse_width += 100
+                print("speed = %d direction = %d" % sample.thruster_pulse_width % sample.servo_pulse_width)
+            elif inp == "h":
+                sample.thruster_pulse_width -= 100
+                print("speed = %d direction = %d"  % sample.thruster_pulse_width % sample.servo_pulse_width)
             elif inp == "stop":
                 sample.thruster_pulse_width = 1100
                 break
             else:
-                print("stop or u or d!")
+                print("stop or u or j or k or h!")
         sample.finalize()
         print("Execution Successed.")
     except KeyboardInterrupt:
